ec2d48238c8483d7e3910a1aade28273cd9ad1f3
epetrov <epetrov@almata.cat>
2022-09-20 17:38:23 +0200

    Hem modificat del Pepe.java, hem afegit 2 linies i hem modificat el main

--- home/development/git/repos/Provaprojecte.git/src/Pepe.java
+++ home/development/git/repos/Provaprojecte.git/src/Pepe.java
@@ -1,5 +1,11 @@
 public class Pepe{ 
 	public static void main(String[] args){ 
 		System.out.println("Comencem el projecte..."); 
+	System.out.println(saluda("pepelu")); 
 	}
  }
+ 
+ public String saluda(String nom) { 
+	return "Hola " + nom + "\n"; 
+}
+
